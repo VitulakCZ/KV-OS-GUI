f9dfb127077d97548be7b6920f02a9a0dd61d5b6
kubik666666 <104312571+kubik666666@users.noreply.github.com>
2023-04-18 18:05:42 +0200

    settings menu a log
    pridal jsem log protoze jsem nevěděl proc se mi to nefunguje tak jsem to tam nechal :D

--- KV_OS.py
+++ KV_OS.py
@@ -3,6 +3,7 @@ import pygame
 from pygame import mixer
 import sys
 import time
+from colorama import init, Fore, Style
 		
 #set title
 pygame.display.set_caption('KV OS')
@@ -15,6 +16,7 @@ mixer.init()
 #into
 mixer.music.load("intro_sound.mp3")
 mixer.music.play(1)
+print(f"{Fore.GREEN}LOG:{Fore.RESET} intro zvuk")
 
 # Initializing surface
 surface = pygame.display.set_mode((800,	600))
@@ -70,6 +72,14 @@ neText = font.render('Ne', True, (64, 0, 64))
 neRect = opravduText.get_rect()
 neRect.center = (X // 1.4, Y // 1.85)
 
+zpetText = font.render('Zpět', True, (64, 0, 64))
+zpetRect = opravduText.get_rect()
+zpetRect.center = (X // 1.4, Y // 1.85)
+
+jazykText = font.render('Jazyk', True, (64, 0, 64))
+jazykRect = opravduText.get_rect()
+jazykRect.center = (X // 2.35, Y // 1.85)
+
 # idk
 font = pygame.font.SysFont('Georgia', 40, bold=True)
 surf = font.render('Shut Down', True, 'white')
@@ -80,25 +90,34 @@ options = pygame.Rect(400, 400, 50, 50)
 ano = pygame.Rect(230, 300, 100, 35)
 ne = pygame.Rect(450, 300, 100, 35)
 OK = pygame.Rect(350, 400, 50, 50)
+zpet = pygame.Rect(450, 300, 100, 35)
+jazyk = pygame.Rect(230, 300, 100, 35)
 
 #
 
 #
 
 vybarveno = False
+zustat = False
+log1 = False
+log2 = False
 
 # main loop
 while running:
+	zustat = False
 	a,b = pygame.mouse.get_pos()
 	
 	# text
 	if not vybarveno:
+		if log1 == False:
+			print(f"{Fore.GREEN}LOG:{Fore.RESET} menu:text")
+			log1 = True
 		surface.blit(vypnoutText, vypnoutRect)
 		surface.blit(optionsText, optionsRect)
 	
 	for events in pygame.event.get():
 		if events.type == pygame.QUIT or (events.type == pygame.MOUSEBUTTONDOWN and shutdown.collidepoint(events.pos)):
-			zustat = False
+			print(f"{Fore.GREEN}LOG:{Fore.RESET} vypnuti:menu")
 			while not zustat:
 				surface.blit(opravduText, opravduRect)
 				# Ano
@@ -120,19 +139,55 @@ while running:
 				for events in pygame.event.get():
 					if events.type == pygame.MOUSEBUTTONDOWN:
 						if ano.collidepoint(events.pos):
+							print(f"{Fore.GREEN}LOG:{Fore.RESET} vypnuti:vypnuti{Fore.RESET}")
 							pygame.quit()
+							print(f"{Fore.RED}SHUTDOWN{Fore.RESET}")
 							quit()
 						if ne.collidepoint(events.pos):
+							print(f"{Fore.GREEN}LOG:{Fore.RESET} vypnuti:zpět")
 							surface.fill(color)
 							zustat = True
 		# options
 		if events.type == pygame.MOUSEBUTTONDOWN:
 			if options.collidepoint(events.pos):
-				surface.blit(pripravaText, pripravaRect)
-				pygame.display.update()
-				time.sleep(1.5)
-				surface.fill(color)
+				print(f"{Fore.GREEN}LOG:{Fore.RESET} settings:menu")
+				while not zustat:
+					#
+					pygame.draw.rect(surface, green, jazyk)
+					surface.blit(jazykText, jazykRect)
+					#
+					pygame.draw.rect(surface, green, zpet)
+					surface.blit(zpetText, zpetRect)
+					#
+					if zpet.x <= a <= zpet.x + 100 and zpet.y <= b <= zpet.y + 35:
+						pygame.draw.rect(surface, color_light, zpet)
+						surface.blit(zpetText, zpetRect)
+					pygame.display.update()
+					for events in pygame.event.get():
+						if events.type == pygame.MOUSEBUTTONDOWN:
+							if zpet.collidepoint(events.pos):
+								print(f"{Fore.GREEN}LOG:{Fore.RESET} settings:zpet")
+								surface.fill(color)
+								zustat = True
+					if jazyk.x <= a <= jazyk.x + 100 and jazyk.y <= b <= jazyk.y + 35:
+						pygame.draw.rect(surface, color_light, jazyk)
+						surface.blit(jazykText, jazykRect)
+					pygame.display.update()
+					for events in pygame.event.get():
+						if events.type == pygame.MOUSEBUTTONDOWN:
+							if jazyk.collidepoint(events.pos):
+								print(f"{Fore.GREEN}LOG:{Fore.RESET} settings:jazyk")
+								surface.blit(pripravaText, pripravaRect)
+								pygame.display.update()
+								time.sleep(1)
+								zustat = True
+								surface.fill(color)
+									
+					#
 		if not vybarveno:
+			if log2 == False:
+				print(f"{Fore.GREEN}LOG:{Fore.RESET} menu:tlacitka")
+				log2 = True
 			if shutdown.x <= a <= shutdown.x + 50 and shutdown.y <=	b <= shutdown.y	+ 50:
 				pygame.draw.rect(surface, color_light, shutdown)
 			else:
